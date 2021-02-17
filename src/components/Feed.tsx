5519732f792d3237123af7ae63ce8d79b056ec3f
hiro-bata <kawa.060313@gmail.com>
2021-02-18 08:39:08 +0900

    post-input

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { auth } from '../firebase';
+import PostInput from './PostInput';
+import styles from './Feed.module.css';
 
 const Feed = () => {
     return (
-        <div>
+        <div className={styles.feed}>
+            <PostInput />
             <button onClick={() => auth.signOut()} >Logout</button>
         </div>
     )

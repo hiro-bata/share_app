f6251cb3ed9fafc6ebaeba672e5757c6151d334c
hiro-bata <kawa.060313@gmail.com>
2021-02-18 06:49:22 +0900

    component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import styles from './App.module.css'
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
+import Feed from './components/Feed';
+import Auth from './components/Auth';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     const unSub = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
@@ -28,9 +32,13 @@ const App: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/feed" component={Feed}></Route>
+        <Route path="/auth" component={Auth}></Route>
+      </Router>      
+    </>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>Hello world!!</h1>
+      <Link to="/feed">Feed</Link><br/>
+      <Link to="/auth">Auth</Link><br/>
+      <Link to="/postaction">PostActon</Link>
+    </>
+  );
+}
+
+export default Home;

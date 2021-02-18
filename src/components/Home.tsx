a80e9a1e17c48dbccf5c737b666bfb99aed8fcf5
hiro-bata <kawa.060313@gmail.com>
2021-02-18 17:32:39 +0900

    home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { auth } from '../firebase';
 
 const Home: React.FC = () => {
   return (
@@ -8,6 +9,8 @@ const Home: React.FC = () => {
       <Link to="/feed">Feed</Link><br/>
       <Link to="/auth">Auth</Link><br/>
       <Link to="/postinput">PostInput</Link><br/>
+      <button onClick={async () => {await auth.signOut()}}>LOGOUT</button>
+      
     </>
   );
 }
